aebfe06b6bf229a1beca7d20088f32be3f46aa51
Gerardo Ramos <gv.rosalesramos@ugto.mx>
2023-01-27 15:53:29 -0600

    iniciamos proyecto

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const PORT = 5000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content.Type', 'text/html')
+    res.end('<h1>Mhehehehehe</h1>')
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Servidor Escuchando: ${PORT}`)
+})
